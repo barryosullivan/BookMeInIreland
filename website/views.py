cb751eb83867416e929d27c6837f526ac3f7d580
barryosullivan <113497392@umail.ucc.ie>
2017-02-15 11:59:08 +0000

    Update
    Update

--- website/views.py
+++ website/views.py
@@ -6,5 +6,6 @@ from django.shortcuts import render, HttpResponse
 #Index Page for the website
 def index( request ):
     context = {
+    'head_title': 'Book Me In Ireland',
     }
     return render( request, 'website/index.html', context )
